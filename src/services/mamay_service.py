9af10f4521589f0849cd08001354f9c9f666ad1e
mechark <forrubbishttp@gmail.com>
2026-03-23 20:45:01 +0200

    feat: add HookedMamay intergration (not sure it works now)

---
+++ src/services/mamay_service.py
@@ -0,0 +1,60 @@
+import torch
+from vllm_hook_plugins import HookedMamay
+from schemas.activations import ActivationPoint
+
+from typing import List
+
+class HookedMamayService:
+    """
+    Service for loading and providing access to the hooked MamayLLM running on vLLM-MamayHook.
+    """
+    def __init__(self):
+        layer_indices = list(range(42))  # Gemma-2-9B has 42 layers
+
+        self.model = HookedMamay(
+            important_layers=layer_indices,
+            gpu_memory_utilization=0.95,
+            max_model_len=2048,
+            trust_remote_code=True,
+            dtype=torch.bfloat16,
+            tensor_parallel_size=1
+        )
+
+    def generate_activations_probing(self, texts: list[str]) -> List[tuple[str, list[ActivationPoint]]]:
+        """
+        Generate text and extract activations for probing.
+
+        Args:
+            texts (list[str]): List of input texts to process.
+
+        Returns:
+            List[tuple[str, list[ActivationPoint]]]: List of tuples containing input text and corresponding activation points.
+
+        Activations will be of shape [hidden_size] for each layer.
+        """
+        all_activations = []
+
+        for text in texts:
+            _, activations_dict = self.model.generate(text) # activations is Dict[layer_idx, Tensor[hidden_size]]
+            activations = []
+            
+            for layer_idx, layer_acts in activations_dict.items():
+                activations.append(
+                    ActivationPoint(
+                        name=f"layer_{layer_idx}",
+                        value=layer_acts
+                    )
+                )
+                
+            all_activations.append((text, activations))
+
+        return all_activations
+    
+if __name__ == "__main__":
+    service = HookedMamayService()
+    texts = ["What is the capital of France?", "Who won the World Cup in 2018?"]
+    activations = service.generate_activations_probing(texts)
+    for text, acts in activations:
+        print(f"Input: {text}")
+        for act in acts:
+            print(f"  {act.name}: shape={act.value.shape}")
